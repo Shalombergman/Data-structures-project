f4398792278155e4f0dfc6514e5453104e9fdc9a
Shalom Bergman <119497630+Shalombergman@users.noreply.github.com>
2024-09-05 14:00:37 +0300

    chanch

--- DataStructuresProject/DefenceStrategiesBST.cs
+++ DataStructuresProject/DefenceStrategiesBST.cs
@@ -1,6 +1,35 @@
-namespace DataStructuresProject;
-
-public class DefenceStrategiesBST
-{
-    
-}
+// namespace DataStructuresProject;
+//
+// public class DefenceStrategiesBST
+// {
+//     private DefenceTreeNode root;
+//
+//     public DefenceStrategiesBST()
+//     {
+//         root = null;
+//     }
+//     public void Insert(int value)
+//     {
+//         root = InsertRecursive(root,  value);
+//         
+//     }
+//     private DefenceTreeNode InsertRecursive(DefenceTreeNode node, int value)
+//     {
+//         if (node == null)
+//         {
+//             node = new DefenceTreeNode(value);
+//             return node;
+//         }
+//
+//         if (value < node.Value)
+//         {
+//             node.Left = InsertRecursive(node.Left, value);
+//         }
+//         else
+//         {
+//             node.Right = InsertRecursive(node.Right, value);
+//         }
+//         return node;
+//     }
+//
+// }
--- DataStructuresProject/DefenceTreeNode.cs
+++ DataStructuresProject/DefenceTreeNode.cs
@@ -2,11 +2,17 @@ namespace DataStructuresProject;
 
 public class DefenceTreeNode
 {
+   
     public  int MinSeverity {get; set;}
     public  int MaxSeverity {get; set;}
     public  List<string> Defenses {get; set;} = new List<string>();
     public  DefenceTreeNode Left {get; set;}
     public  DefenceTreeNode Right {get; set;}
+    //public Dictionary<string, int,string,int,string,List<string>> Children { get; set; } = new Dictionary<string, DefenceTreeNode>();
+    // public DefenceTreeNode(Dictionary<DefenceTreeNode, DefenceTreeNode> dict)
+    // {
+    //     
+    // }
 
     public DefenceTreeNode(int minSeverity, int maxSeverity ,List<string> defenses)
     {
