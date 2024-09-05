51f8381f717928035d8413f2e4521e7542c84c0e
Shalom Bergman <119497630+Shalombergman@users.noreply.github.com>
2024-09-05 14:05:38 +0300

    PROJECT SULUSION

---
+++ DataStructuresProject/Program.cs
@@ -0,0 +1,36 @@
+﻿
+
+using DataStructuresProject;
+//using Newtonsoft.Json;
+using System.Text.Json;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<DefenceTreeNode> defenceTreeNodes = ReadFromJson<List<DefenceTreeNode>>("/Users/shalom_bergman/kodcode2/Data_engineering_course/DataStructuresProject/DataStructuresProject/Json/defenceStrategies.json");  
+
+        static T ReadFromJson<T>(string filePath)
+        {
+            string jsonString = File.ReadAllText(filePath);
+            return JsonSerializer.Deserialize<T>(jsonString);
+        }
+
+        foreach (DefenceTreeNode defenceTreeNode in defenceTreeNodes)
+        {
+            Console.WriteLine(defenceTreeNode.MaxSeverity);
+        }
+        //Console.WriteLine($"\n{defenceTreeNodes.ToString()}");
+        // string jsonString = JsonSerializer.Serialize(destination, new JsonSerializerOptions() { WriteIndented = true});  
+        // using (StreamWriter outputFile = new StreamWriter("dataReady.json"))  
+        // {  
+        //     outputFile.WriteLine(jsonString);  
+        // }  
+
+        // using (StreamReader r = new StreamReader())  
+        // {  
+        //     string json = r.ReadToEnd();  
+        //     defenceTreeNodes = JsonSerializer.Deserialize<List<DefenceTreeNode>>(json);  
+        // }
+    }
+}
